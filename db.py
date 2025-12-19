a5140b633a400e4c16310b78f26d601d00843ae9
Thithiksha <727722eucb063@skcet.ac.in>
2025-12-19 15:30:22 +0530

    Use Supabase pooler to force IPv4 on Streamlit Cloud

--- db.py
+++ db.py
@@ -5,7 +5,8 @@ import streamlit as st
 def get_conn():
     return psycopg2.connect(
         st.secrets["DATABASE_URL"],
-        sslmode="require"
+        sslmode="require",
+        connect_timeout=5
     )
 
 
